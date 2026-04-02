8f63956150c8973c39b504632bdbcd09e2ffa66b
sreehari-ss <sreehari.prasad@suprsend.com>
2026-04-02 14:21:21 +0530

    eng-390-replace-deprecated-tenant-apis (#22)
    * feat: replace deprecated tenant API methods with new implementations
    
    ---------
    
    Co-authored-by: skme902 <skme902@gmail.com>

--- tenant_api.go
+++ tenant_api.go
@@ -12,7 +12,12 @@ type TenantsService interface {
 	Upsert(context.Context, string, *Tenant) (*Tenant, error)
 	List(context.Context, *TenantListOptions) (*TenantList, error)
 	Delete(context.Context, string) error
+	ListPreferenceCategories(context.Context, string, *TenantCategoriesPreferenceOptions) (*TenantCategoriesPreferenceResponse, error)
+	GetPreferenceCategory(context.Context, string, string, *TenantPreferenceCategoryOptions) (*TenantCategoryPreference, error)
+	UpdatePreferenceCategory(context.Context, string, string, TenantPreferenceCategoryUpdateBody, *TenantPreferenceCategoryOptions) (*TenantCategoryPreference, error)
+	// Deprecated: Use ListPreferenceCategories instead.
 	GetAllCategoriesPreference(context.Context, string, *TenantCategoriesPreferenceOptions) (*TenantCategoriesPreferenceResponse, error)
+	// Deprecated: Use UpdatePreferenceCategory instead.
 	UpdateCategoryPreference(context.Context, string, string, TenantCategoryPreferenceUpdateBody) (*TenantCategoryPreference, error)
 }
 
@@ -142,9 +147,12 @@ type TenantCategoryPreference struct {
 	RootCategory             string   `json:"root_category"`
 	DefaultPreference        string   `json:"default_preference"`
 	DefaultMandatoryChannels []string `json:"default_mandatory_channels"`
+	DefaultOptInChannels     []string `json:"default_opt_in_channels"`
+	EnabledForTenant         bool     `json:"enabled_for_tenant"`
 	VisibleToSubscriber      bool     `json:"visible_to_subscriber"`
 	Preference               *string  `json:"preference"`
 	MandatoryChannels        []string `json:"mandatory_channels"`
+	OptInChannels            []string `json:"opt_in_channels"`
 	BlockedChannels          []string `json:"blocked_channels"`
 	Tags                     []string `json:"tags"`
 	EffectiveTags            []string `json:"effective_tags"`
@@ -154,6 +162,9 @@ type TenantCategoriesPreferenceOptions struct {
 	Limit  int
 	Offset int
 	Tags   string
+	Locale string
+	//
+	IncludeDisabled bool
 }
 
 func (opts *TenantCategoriesPreferenceOptions) BuildQuery() string {
@@ -168,10 +179,98 @@ func (opts *TenantCategoriesPreferenceOptions) BuildQuery() string {
 		if opts.Tags != "" {
 			query.Set("tags", opts.Tags)
 		}
+		if opts.Locale != "" {
+			query.Set("locale", opts.Locale)
+		}
+		if opts.IncludeDisabled {
+			query.Set("include_disabled", strconv.FormatBool(opts.IncludeDisabled))
+		}
 	}
 	return query.Encode()
 }
 
+func (t *tenantsService) ListPreferenceCategories(ctx context.Context, tenantId string, opts *TenantCategoriesPreferenceOptions) (*TenantCategoriesPreferenceResponse, error) {
+	urlStr := appendQueryParamPart(fmt.Sprintf("%spreference/category/", t.tenantAPIUrl(tenantId)), opts.BuildQuery())
+	request, err := t.client.prepareHttpRequest("GET", urlStr, nil)
+	if err != nil {
+		return nil, err
+	}
+	httpResponse, err := t.client.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+	resp := &TenantCategoriesPreferenceResponse{}
+	err = t.client.parseApiResponse(httpResponse, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+type TenantPreferenceCategoryOptions struct {
+	Locale string
+}
+
+func (opts *TenantPreferenceCategoryOptions) BuildQuery() string {
+	query := url.Values{}
+	if opts != nil {
+		if opts.Locale != "" {
+			query.Set("locale", opts.Locale)
+		}
+	}
+	return query.Encode()
+}
+
+func (t *tenantsService) GetPreferenceCategory(ctx context.Context, tenantId, category string, opts *TenantPreferenceCategoryOptions) (*TenantCategoryPreference, error) {
+	urlStr := appendQueryParamPart(fmt.Sprintf("%spreference/category/%s/", t.tenantAPIUrl(tenantId), url.PathEscape(category)), opts.BuildQuery())
+	request, err := t.client.prepareHttpRequest("GET", urlStr, nil)
+	if err != nil {
+		return nil, err
+	}
+	httpResponse, err := t.client.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+	resp := &TenantCategoryPreference{}
+	err = t.client.parseApiResponse(httpResponse, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+type TenantPreferenceCategoryUpdateBody struct {
+	EnabledForTenant *bool    `json:"enabled_for_tenant,omitempty"`
+	BlockedChannels  []string `json:"blocked_channels"`
+	//
+	VisibleToSubscriber *bool    `json:"visible_to_subscriber,omitempty"`
+	Preference          *string  `json:"preference,omitempty"`
+	MandatoryChannels   []string `json:"mandatory_channels"`
+	OptInChannels       []string `json:"opt_in_channels"`
+}
+
+func (t *tenantsService) UpdatePreferenceCategory(ctx context.Context, tenantId, category string, body TenantPreferenceCategoryUpdateBody, opts *TenantPreferenceCategoryOptions) (*TenantCategoryPreference, error) {
+	urlStr := appendQueryParamPart(fmt.Sprintf("%spreference/category/%s/", t.tenantAPIUrl(tenantId), url.PathEscape(category)), opts.BuildQuery())
+	request, err := t.client.prepareHttpRequest("PATCH", urlStr, body)
+	if err != nil {
+		return nil, err
+	}
+	httpResponse, err := t.client.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+	resp := &TenantCategoryPreference{}
+	err = t.client.parseApiResponse(httpResponse, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// Deprecated: Use ListPreferenceCategories instead.
 func (t *tenantsService) GetAllCategoriesPreference(ctx context.Context, tenantId string, opts *TenantCategoriesPreferenceOptions) (*TenantCategoriesPreferenceResponse, error) {
 	urlStr := appendQueryParamPart(fmt.Sprintf("%scategory/", t.tenantAPIUrl(tenantId)), opts.BuildQuery())
 	request, err := t.client.prepareHttpRequest("GET", urlStr, nil)
@@ -198,6 +297,7 @@ type TenantCategoryPreferenceUpdateBody struct {
 	BlockedChannels     []string `json:"blocked_channels,omitempty"`
 }
 
+// Deprecated: Use UpdatePreferenceCategory instead.
 func (t *tenantsService) UpdateCategoryPreference(ctx context.Context, tenantId, category string, body TenantCategoryPreferenceUpdateBody) (*TenantCategoryPreference, error) {
 	urlStr := fmt.Sprintf("%scategory/%s/", t.tenantAPIUrl(tenantId), url.PathEscape(category))
 	request, err := t.client.prepareHttpRequest("PATCH", urlStr, body)
